7bd728d09b8de7eb875177d66939587b56203570
lpercivalDEV <lpercival.dev@gmail.com>
2018-11-04 22:25:50 -0500

    first commit - functionality complete, style still in progress

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+let user = document.getElementById("firstName");
+let choice = document.getElementById("background");
+let printResult = document.getElementById("result");
+
+
+document.getElementById('background').onchange = function(){
+    body.style.backgroundColor = background.value;
+    switch (choice.value) {
+        case "#B03F3B":
+            console.log("You selected Brick.");
+            console.log(user);
+            printResult.innerHTML=  user.value + ' selected Brick. Nice one!';
+            break;
+            case "#94A437":
+            console.log("You selected Bamboo Green.");
+            printResult.innerHTML=  user.value + ' selected Bamboo Green. Good choice!';
+                break;
+                case "#629CCB":
+                console.log("You selected Ice Blue.");
+                printResult.innerHTML=  user.value + ' selected Ice Blue. Super cool!';
+                    break;
+                    case "#7D5EC9":
+                    console.log("You selected Grape.");
+                    printResult.innerHTML=  user.value + ' selected Grape. Tasty!';
+                        break;
+                        case "#67224F":
+                        console.log("You selected Wine.");
+                        printResult.innerHTML=  user.value + ' selected Wine. Divine!';
+                            break;
+                            case "#774228":
+                            console.log("You selected Coffee.");
+                            printResult.innerHTML=  user.value + ' selected Coffee. Best choice!';
+                                break;
+        default: printResult.innerHTML=  user.value + ", please select a color from the dropdown."
+
+    }
+};
